a9480477a9f14bfa644e514284867458af2bc749
Abigail Demsas <ademsas1026@gmail.com>
2018-07-05 11:48:46 -0400

    some changes

--- server/api/books.js
+++ server/api/books.js
@@ -1,23 +1,4 @@
-const bookRouter = require('express').Router()
-const {Book} = require('../db')
-module.exports = bookRouter
 
-bookRouter.get('/', async (req, res, next) => {
-  try {
-    const books = await Book.findAll();
-    res.send(books);
-  } catch (err){
-    next(err)
-  }
-})
 
-bookRouter.get('/:bookId', async (req, res, next) => {
-  try {
-    const book = await Book.findById(req.params.bookId);
-    res.send(book);
-  } catch (err){
-    next(err)
-  }
-})
 
 
